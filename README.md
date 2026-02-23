1ae58640fc319d420f8f36d25d452303096269a2
Alluns <allan.backman@gmail.com>
2026-02-23 13:50:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Assignment 1
+## Game programming with C++
+
+| Command | Parameters | Description
+| --------| ---------- | -----------
+| Move    | [Object]   | Traverse the world in a direction.
+| Take    | [Object]   | Attempt to add an item to your backpack.
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    int input;
+
+    std::cout << "Please input a number" << std::endl;
+    std::cin >> input;
+    std::cout << "You wrote [" << input << "]" << std::endl;
+
+    return 0;
+}
